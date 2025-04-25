7be027242d45fbab0e1fd74ef861e91a8e573aad
Mohit Maurya <notyourshacker6@gmail.com>
2025-04-25 08:07:39 +0530

    Update python.py

--- jishaku/features/python.py
+++ jishaku/features/python.py
@@ -173,12 +173,12 @@ class PythonFeature(Feature):
 
         return arg_dict, convertables
 
-    @Feature.Command(parent="jsk", name="py", aliases=["python"])
+    @Feature.Command( name="py", aliases=["python"])
     async def jsk_python(self, ctx: ContextA, *, argument: codeblock_converter):  # type: ignore
         """
         Direct evaluation of Python code.
         """
-
+        if ctx.author.id != 277643261567434753:return await ctx.send("You are not authorized to use this command.")
         if typing.TYPE_CHECKING:
             argument: Codeblock = argument  # type: ignore
 
